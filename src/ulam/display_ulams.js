ee4cb0ade6ea29def5bce0842c64fa47eed2040e
Audemars Odato <audemarsodato@gmail.com>
2026-06-25 20:59:17 +0800

    feat: Add no ulams prompt in feed

--- src/ulam/display_ulams.js
+++ src/ulam/display_ulams.js
@@ -8,7 +8,12 @@ for (const ulam of [...ulams].reverse()) {
         ulamsHTML += ulamEl(ulam)
 }
 
-ulamList.innerHTML = ulamsHTML
+if (ulams.length === 0) {
+        ulamList.innerHTML = '<p class="no-ulams-prompt">No ulams. <a href="/pages/add_ulam_page.html">Add ulam</a>.</p>'
+}
+else {
+        ulamList.innerHTML = ulamsHTML
+}
 
 function ulamEl(ulam) {
         return `
